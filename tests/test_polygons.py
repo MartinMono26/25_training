f73b4cc1e2f70a2180417d04f908600a71c24482
MC_Baby <martinmono26@gmail.com>
2025-05-15 15:29:47 +0200

    adding parent of test directory to path in the test files

--- tests/test_polygons.py
+++ tests/test_polygons.py
@@ -1,4 +1,6 @@
-from polygons import Polygon, Square
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from polygons import Square
 
 class TestPolygon:
 
@@ -6,3 +8,9 @@ class TestPolygon:
         s=Square( name = 's1', side_length_mm = 5)
         s.compute_area()
         assert (s.area==25)
+
+    def test_pif(self):
+        assert (True)
+
+    def test_paf(self):
+        assert (True)
