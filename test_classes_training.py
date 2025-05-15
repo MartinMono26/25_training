21880f79d3ce209146214c6406009bd0bb4755b4
MC_Baby <martinmono26@gmail.com>
2025-05-15 11:58:37 +0200

    adding test test_get_volume

--- test_classes_training.py
+++ test_classes_training.py
@@ -1,9 +1,15 @@
-import pytest
+from classes_training import Cylinder
 
-class TestVolume:
+class TestCylinder:
 
-    def test_toto(self):
-        assert(True)
+    def test_get_volume(self):
+        c1 = Cylinder(
+            name='c1',
+            height=2,
+            diameter=4
+        )
+        v = c1.get_volume()
+        assert(v==25.132741228718345)
 
     def test_tata(self):
         assert(False)
